d0bed0d69ac0f06e6dfc2ecd39b724889a9a7ff2
coding <codingbuilder@gmail.com>
2024-08-14 05:30:01 +0800

    Update pixels.js

--- pixels.js
+++ pixels.js
@@ -13118,7 +13118,7 @@
                 return l
             }
         });
-        let r = parseFloat("8.25")
+        let r = parseFloat("8.3")
           , i = 1676498533e3
           , o = "game-container"
           , a = "players"
